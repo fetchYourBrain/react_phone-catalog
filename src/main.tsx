592a7f50acbd04c076de8ccdfa43343d054dcbc7
Maksym <max.borovique@gmail.com>
2024-09-12 18:37:20 +0300

    add initial styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { App } from "./App.tsx";
+import "./index.scss";
 
 createRoot(document.getElementById("root")!).render(<App />);

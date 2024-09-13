35e3ea2f29f2815ee899daa36aa8a187eec041a2
Maksym <max.borovique@gmail.com>
2024-09-13 19:32:21 +0300

    add hover and active

--- src/components/NavItem/NavItem.tsx
+++ src/components/NavItem/NavItem.tsx
@@ -1,13 +1,22 @@
-interface Props {
-  item: string;
-}
-export const NavItem: React.FC<Props> = ({ item }) => {
-  return (
-    <li>
-    {/* <a href={`#${item}`} className={getActiveLink}> */}
-    <a href={`#${item}`}>
-      {item}
-    </a>
-  </li>
-  );
-}
+  import classNames from "classnames";
+  import styles from './NavItem.module.scss';
+  import { NavLink } from "react-router-dom";
+
+  interface Props {
+    item: string;
+  }
+
+  const styledActive = ({isActive}: {isActive: boolean}) => {
+    return classNames(styles.navLink, {
+      [styles.active]: isActive,
+    })
+  }
+  export const NavItem: React.FC<Props> = ({ item }) => {
+    return (
+      <li>
+      <NavLink to={item} className={styledActive}>
+        {item}
+      </NavLink>
+    </li>
+    );
+  }

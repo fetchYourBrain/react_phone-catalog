564857590c13b0ce4365ae664a866ca6ec1ad098
Roman Bekh <coldshein@gmail.com>
2024-09-13 13:09:58 +0300

    created store

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.scss";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);

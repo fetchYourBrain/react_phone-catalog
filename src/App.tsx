638279ad5b440513a73c814686199e07b4df6772
Maksym <max.borovique@gmail.com>
2024-09-15 19:52:53 +0300

    add cart page(mobile only)

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,14 @@ import "./reset.scss";
 import { Header } from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
 import { Container } from "./components/Container/Container";
-import { CartPage } from "./pages/CartPage/CartPage";
+
 export const App = () => {
   return (
     <>
       <Header />
-      {/* TO DELETE! */}
-      <CartPage />
-      {/* <Container>
+      <Container>
         <Outlet />
-      </Container> */}
+      </Container>
     </>
   );
 };
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,19 +1,33 @@
 import { CartItem } from "../../components/CartItem/CartItem";
-import styles from './CartPage.module.scss';
+import { GoBackButton } from "../../components/Buttons/GoBackButton/GoBackButton";
+import styles from "./CartPage.module.scss";
+import { CartButton } from "../../components/Buttons/CartButton/CartButton";
 
-const testingStorageArray: number[] = [];
+const testingStorageArray: number[] = [1, 2, 3, 4, 5];
 
 export const CartPage = () => {
   return (
-    <div className="cart">
-      <div className="container">
-        <div className="back_button">
-          <button className="button">Back</button>
-          <div className="cart-items-block">
-            {testingStorageArray.map((item) => (
-              <CartItem item={item} key={item}/>
-            ))}
+    <div className={styles.cart}>
+      <div className={styles.container}>
+        <GoBackButton />
+
+        <h1 className={styles.title}>Cart</h1>
+        <div className="cart-items-block">
+          {testingStorageArray.map((item) => (
+            <CartItem item={item} key={item} />
+          ))}
+        </div>
+        <div className={styles.total}>
+          <div className="total__cost">
+            <h2 className={styles.number}>$2657</h2>
           </div>
+
+          <div className={styles.total__items}>
+            <span className={styles.items}>Total for 3 items</span>
+          </div>
+
+          <hr className={styles.line} />
+          <CartButton />
         </div>
       </div>
     </div>

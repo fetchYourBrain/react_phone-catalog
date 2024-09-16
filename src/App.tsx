b1d25ce033ce8deb90f13e2a5f2122ae69c9f380
Maksym <max.borovique@gmail.com>
2024-09-16 18:34:27 +0300

    add cart item component

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,14 @@ import "./reset.scss";
 import { Header } from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
 import { Container } from "./components/Container/Container";
-import { CartItem } from "./components/Cart/CartItem";
+
 export const App = () => {
   return (
     <>
       <Header />
-      <CartItem />
-      {/* <Container>
+      <Container>
         <Outlet />
-      </Container> */}
+      </Container>
     </>
   );
 };
--- src/components/Cart/CartItem.tsx
+++ src/components/Cart/CartItem.tsx
@@ -1,36 +1,65 @@
-import { AddButton } from "../AddButton/AddButton";
+import { useState } from "react";
 import styles from "./CartItem.module.scss";
+import classNames from "classnames";
 
 export const CartItem = () => {
+  const [count, setCount] = useState(1);
+  const price = 999;
+  const value = count * price;
   return (
     <div className={styles.cart_item}>
       <div className={styles.container}>
-        <button className={styles.button}>
-        <img
-            className={styles.button_image}
-            src="../public/img/icons/close_card_item_button.svg"
-          />
-        </button>
-        <div className={styles.image_container}>
-          <img
-            className={styles.image}
-            src="../public/img/phones/apple-iphone-14/purple/00.webp"
-            alt="Image of the phone"
-          />
-        </div>
-        <h3 className={styles.title}>Apple iPhone 14 Pro 128GB Silver (MQ023)</h3>
-      </div>
-
-      <div className={styles.amount}>
-        <div className={styles.counter}>
-          <div className={styles.change}>
-            <img src="../public/img/icons/minus-icon.svg" alt="Minus amount button" />
+        <div className={styles.item}>
+          <button className={styles.button} aria-label="Remove item">
+            <div className={styles.button_image} />
+          </button>
+          <div className={styles.image_container}>
+            <img
+              className={styles.image}
+              src="../public/img/phones/apple-iphone-14/purple/00.webp"
+              alt="Image of the phone"
+            />
           </div>
-          <div className={styles.count}>1</div>
-        <AddButton />
+          <h3 className={styles.title}>
+            Apple iPhone 14 Pro 128GB Silver (MQ023)
+          </h3>
         </div>
-        <div className="value">
-          <p>$999</p>
+        <div className={styles.amount}>
+          <div className={styles.counter}>
+            <div
+              className={classNames(styles.subtract_container, {
+                [styles.active]: count > 1,
+              })}
+            >
+              <button
+                disabled={count <= 1}
+                aria-label="Decrease quantity"
+                className={classNames(styles.change_button, {
+                  [styles.active]: count > 1,
+                })}
+                onClick={() => setCount((prev) => prev - 1)}
+              >
+                <div
+                  className={classNames(styles.subtract, {
+                    [styles.active]: count > 1,
+                  })}
+                ></div>
+              </button>
+            </div>
+            <div className={styles.count}>{count}</div>
+            <div className={styles.add_container}>
+              <button
+              aria-label="Increase quantity"
+                className={styles.change_button}
+                onClick={() => setCount((prev) => prev + 1)}
+              >
+                <div className={styles.add}></div>
+              </button>
+            </div>
+          </div>
+          <div className={styles.value}>
+            <h3>${value}</h3>
+          </div>
         </div>
       </div>
     </div>

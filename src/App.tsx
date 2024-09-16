fe80b7a30fa7e54dbc8217fa7418e4b1629c925c
Maksym <max.borovique@gmail.com>
2024-09-16 20:05:32 +0300

    add new folders and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./App.scss";
-import "./reset.scss";
 import { Header } from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
 import { Container } from "./components/Container/Container";

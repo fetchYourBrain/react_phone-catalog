cee162cd8634d672f3292f464d24a7397312408d
Vasyl Mazurok <vasok1991@gmail.com>
2024-09-16 13:54:59 +0300

    delete not found component

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,14 @@ import "./reset.scss";
 import { Header } from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
 import { Container } from "./components/Container/Container";
-import { NotFound } from "./components/NotFound/NotFound";
+
 export const App = () => {
   return (
     <>
-    <NotFound />
-      {/* <Header />
+      <Header />
       <Container>
         <Outlet />
-      </Container> */}
+      </Container>
     </>
   );
 };

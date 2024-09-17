183cd512b324071f380e2ec41ac91b62ea4d4cf4
Maksym <max.borovique@gmail.com>
2024-09-17 18:05:15 +0300

    add responsive cart

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,33 +1,37 @@
-import { CartItem } from "../../components/CartItem/CartItem";
 import { GoBackButton } from "../../components/Buttons/GoBackButton/GoBackButton";
 import styles from "./CartPage.module.scss";
-import { CartButton } from "../../components/Buttons/CartButton/CartButton";
+import { CartItem } from "../../components/Cart/CartItem";
 
 const testingStorageArray: number[] = [1, 2, 3, 4, 5];
 
 export const CartPage = () => {
   return (
     <div className={styles.cart}>
-      <div className={styles.container}>
+      <div className={styles.inner}>
         <GoBackButton />
 
         <h1 className={styles.title}>Cart</h1>
-        <div className="cart-items-block">
-          {testingStorageArray.map((item) => (
-            <CartItem item={item} key={item} />
-          ))}
-        </div>
-        <div className={styles.total}>
-          <div className="total__cost">
-            <h2 className={styles.number}>$2657</h2>
+        <div className={styles.cart_block_container}>
+          <div>
+            {testingStorageArray.map((item) => (
+              <CartItem key={item} />
+            ))}
           </div>
+          <div className={styles.total}>
+            <div className="total__cost">
+              <h2 className={styles.number}>$2657</h2>
+            </div>
 
-          <div className={styles.total__items}>
-            <span className={styles.items}>Total for 3 items</span>
+            <div className={styles.total__items}>
+              <span className={styles.items}>Total for 3 items</span>
+            </div>
+
+            <hr className={styles.line} />
+
+            <button className={styles.main_button}>
+              <p className={styles.button_text}>Checkout</p>
+            </button>
           </div>
-
-          <hr className={styles.line} />
-          <CartButton />
         </div>
       </div>
     </div>

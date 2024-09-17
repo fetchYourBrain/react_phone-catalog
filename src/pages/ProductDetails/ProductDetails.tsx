af4140adf26c61eee519c0938adad3f3b414b1c6
Roman Bekh <coldshein@gmail.com>
2024-09-18 02:50:41 +0300

    fixed

--- src/pages/ProductDetails/ProductDetails.tsx
+++ src/pages/ProductDetails/ProductDetails.tsx
@@ -1,12 +1,10 @@
 import { useParams } from "react-router-dom";
 import { useAppSelector } from "../../hooks/helperToolkit";
 import styles from "./ProductDetails.module.scss";
-import { ProductsRow } from "../../components/ProductsRow/ProductsRow";
 
 export const ProductDetails = () => {
   const { id } = useParams<{ id: string }>();
   const products = useAppSelector((state) => state.productList.products);
-  const recommended = useAppSelector((state) => state.product.products);
 
   const visibleProduct = products.find((product) => product.id === id);
 

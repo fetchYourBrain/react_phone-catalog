e6a7bfad33367119e885144b1cff26471ca0e031
Vasyl Mazurok <vasok1991@gmail.com>
2024-09-23 11:21:03 +0300

    add layout

--- src/pages/ProductDetails/ProductDetails.tsx
+++ src/pages/ProductDetails/ProductDetails.tsx
@@ -2,69 +2,71 @@ import React, { useEffect, useState } from "react";
 import cn from "classnames";
 import { useNavigate, useParams } from "react-router-dom";
 import styles from "./ProductDetails.module.scss";
-import { useAppSelector } from "../../hooks/helperToolkit";
+import { useAppDispatch, useAppSelector } from "../../hooks/helperToolkit";
 import { GoBackButton } from "../../components/Buttons/GoBackButton/GoBackButton";
 import { Breadcrumbs } from "../../components/Breadcrumbs/Breadcrumbs";
+import { fetchDeviceById, fetchDevicesList } from "../../slices/deviceSlice";
+import { Loader } from "../../components/Loader/Loader";
 
 export const ProductDetails: React.FC = () => {
-  const { id } = useParams<{ id: string }>();
+  const { id, category } = useParams<{ id: string; category: string }>();
+  const dispatch = useAppDispatch();
+  const { deviceById, devices } = useAppSelector((state) => state.device);
   const navigate = useNavigate();
-  const products = useAppSelector((state) => state.device.devices);
 
-  const visibleProduct = products.find((product) => product.itemId === id);
-
-  console.log(id);
-  
-  const {
-    name,
-    images,
-    priceRegular,
-    priceDiscount,
-    color,
-    colorsAvailable,
-    capacity,
-    capacityAvailable,
-    screen,
-    resolution,
-    processor,
-    ram,
-  } = visibleProduct;
-
+  console.log(devices);
 
   // Стан для вибраного кольору, ємності та головного зображення
-  const [selectedColor, setSelectedColor] = useState(color);
-  const [selectedCapacity, setSelectedCapacity] = useState(capacity);
-  const [mainImage, setMainImage] = useState(images[0]);
+  const [selectedColor, setSelectedColor] = useState<string>("");
+  const [selectedCapacity, setSelectedCapacity] = useState<string>("");
+  const [mainImage, setMainImage] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
-  // Функція для пошуку продукту за новим кольором та ємністю
-  const findProductByAttributes = (newColor: string, newCapacity: string) => {
-    return products.find(
-      (product) => product.color === newColor && product.capacity === newCapacity
-    );
-  };
+  const currentDevice = devices.find(
+    (device) => device.itemId === deviceById?.id
+  );
+
+  // Встановлюємо значення для кольору, ємності та головного зображення при зміні deviceById
+  useEffect(() => {
+    if (deviceById) {
+      setSelectedColor(deviceById.color || "");
+      setSelectedCapacity(deviceById.capacity || "");
+      setMainImage(deviceById.images[0] || "");
+      setLoading(false);
+    }
+  }, [deviceById]);
+
+  // Завантаження пристроїв при зміні id або категорії
+  useEffect(() => {
+    if (category) {
+      setLoading(true);
+      dispatch(fetchDevicesList(category));
+    }
+    dispatch(fetchDeviceById({ id, category }));
+  }, [id, category, dispatch]);
 
   // Функція для обробки зміни кольору або ємності і перенаправлення на нову сторінку
   const handleProductChange = (newColor: string, newCapacity: string) => {
-    const newProduct = findProductByAttributes(newColor, newCapacity);
-
-    if (newProduct) {
-      // Оновлюємо стан вибраного кольору та ємності
-      setSelectedColor(newColor);
-      setSelectedCapacity(newCapacity);
-
-      // Навігація на нову сторінку з правильним id
-      navigate(`/product/${newProduct.id}`);
+    if (newColor && newCapacity && deviceById?.namespaceId) {
+      const newId = `${deviceById.namespaceId}-${newCapacity.toLowerCase()}-${newColor.split(' ').join('-')}`;
+      navigate(`/${category}/${newId}`);
     }
   };
 
   // Обробка зміни кольору
   const handleColorChange = (newColor: string) => {
-    handleProductChange(newColor, selectedCapacity);
+    if (newColor !== selectedColor) {
+      setSelectedColor(newColor);
+      handleProductChange(newColor, selectedCapacity);
+    }
   };
 
   // Обробка зміни ємності
   const handleCapacityChange = (newCapacity: string) => {
-    handleProductChange(selectedColor, newCapacity);
+    if (newCapacity !== selectedCapacity) {
+      setSelectedCapacity(newCapacity);
+      handleProductChange(selectedColor, newCapacity);
+    }
   };
 
   // Обробка кліку по мініатюрі зображення
@@ -72,63 +74,74 @@ export const ProductDetails: React.FC = () => {
     setMainImage(image);
   };
 
-    // Встановлюємо перше зображення як головне
-    useEffect(() => {
-      if (visibleProduct) {
-        setMainImage(visibleProduct.images[0]);
-      }
-    }, [visibleProduct]);
+  // Функція для перевірки дійсності кольору
+  const isValidCssColor = (color: string) => {
+    const s = new Option().style;
+    s.color = color;
+    return s.color !== '';
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+
 
   return (
-    <div className={styles.block}>
-      <Breadcrumbs name={visibleProduct?.name} />
+    <>
+      <Breadcrumbs name={deviceById?.name} />
       <GoBackButton />
-      <h2 className={styles.title}>
-        {name}
-      </h2>
+      <div className={styles.block}>
+        <h2 className={styles.title}>{deviceById?.name}</h2>
 
-      <ul className={styles.wrapper}>
-        <li className={styles.container_image}>
+        <div className={styles.container_image}>
           <img
             src={mainImage}
-            alt={name}
+            alt={deviceById?.name}
             className={styles.image}
           />
-        </li>
-        <li>
-          <ul className={styles.collection}>
-            {images.map((image) => (
-              <li
-                key={image}
-                className={cn(styles.collection_item,
-                  { [styles.is_active]: image === mainImage })}
-                onClick={() => handleImageClick(image)}
-              >
-                <img
-                  src={image}
-                  alt={name}
-                  className={styles.image}
-                />
-              </li>
-            ))}
-          </ul>
-        </li>
-        <li>
+        </div>
+
+        <ul className={styles.collection}>
+          {deviceById?.images.map((image) => (
+            <li
+              key={image}
+              className={cn(styles.collection_item, {
+                [styles.is_active]: image === mainImage,
+              })}
+              onClick={() => handleImageClick(image)}
+            >
+              <img
+                src={image}
+                alt={deviceById?.name}
+                className={styles.image}
+              />
+            </li>
+          ))}
+        </ul>
+        
+        <div className={styles.variant}>
           <div className={styles.colors_title_box}>
             <p className={styles.colors_title}>Available colors</p>
-            <p className={styles.product_id}>ID: {visibleProduct.id}</p>
+            <p className={styles.product_id}>
+              ID: {currentDevice?.id?.toString().padStart(6, "0")}
+            </p>
           </div>
           <ul className={styles.color_list}>
-            {colorsAvailable.map(colorOption => (
+            {deviceById?.colorsAvailable.map((colorOption) => (
               <li
-                key={colorOption} 
-                className={cn(styles.color_wrapper,
-                  { [styles.active_color]: colorOption === selectedColor })}
+                key={colorOption}
+                className={cn(styles.color_wrapper, {
+                  [styles.active_color]: colorOption === selectedColor,
+                })}
                 onClick={() => handleColorChange(colorOption)}
               >
-                <div 
+                <div
                   className={styles.color_item}
-                  style={{ backgroundColor: colorOption }}
+                  style={{
+                    backgroundColor: isValidCssColor(colorOption)
+                      ? colorOption
+                      : "black",
+                  }}
                 >
                 </div>
               </li>
@@ -137,11 +150,12 @@ export const ProductDetails: React.FC = () => {
 
           <p className={styles.capacity_title}>Select capacity</p>
           <ul className={styles.capacity_list}>
-            {capacityAvailable.map(memory => (
+            {deviceById?.capacityAvailable.map((memory) => (
               <li
                 key={memory}
-                className={cn(styles.capacity_item,
-                  { [styles.active_capacity]: memory === selectedCapacity })}
+                className={cn(styles.capacity_item, {
+                  [styles.active_capacity]: memory === selectedCapacity,
+                })}
                 onClick={() => handleCapacityChange(memory)}
               >
                 {memory}
@@ -150,46 +164,42 @@ export const ProductDetails: React.FC = () => {
           </ul>
 
           {/* Ціна та кнопка */}
-            <div className={styles.price}>
-              <p className={styles.discount_price}>${priceDiscount}</p>
-              <p className={styles.regular_price}>${priceRegular}</p>
-            </div>
-            <div className={styles.buttons}>
-              <a href="#" className={styles.add_to_cart_button}>
-                Add to cart
-              </a>
-              <a href="#" className={styles.heart_icon_button}>
-                <img src="/img/icons/heart-icon.svg" alt="Heart Icon" />
-              </a>
-            </div>
+          <div className={styles.price}>
+            <p className={styles.discount_price}>
+              ${deviceById?.priceDiscount}
+            </p>
+            <p className={styles.regular_price}>${deviceById?.priceRegular}</p>
+          </div>
+          <div className={styles.buttons}>
+            <button className={styles.add_to_cart_button}>
+              Add to cart
+            </button>
+            <button className={styles.heart_icon_button}>
+              <img src="public\img\icons\heart-icon.svg" alt="Heart Icon" />
+            </button>
+          </div>
 
           {/* Характеристики телефону */}
           <section className={styles.specs}>
             <div className={styles.specs_box}>
               <p className={styles.specs_title}>Screen:</p>
-              <p className={styles.specs_desc}>{screen}</p>
+              <p className={styles.specs_desc}>{deviceById?.screen}</p>
             </div>
             <div className={styles.specs_box}>
               <p className={styles.specs_title}>Resolution:</p>
-              <p className={styles.specs_desc}>{resolution}</p>
+              <p className={styles.specs_desc}>{deviceById?.resolution}</p>
             </div>
             <div className={styles.specs_box}>
               <p className={styles.specs_title}>Processor:</p>
-              <p className={styles.specs_desc}>{processor}</p>
+              <p className={styles.specs_desc}>{deviceById?.processor}</p>
             </div>
             <div className={styles.specs_box}>
               <p className={styles.specs_title}>RAM: </p>
-              <p className={styles.specs_desc}>{ram}</p>
+              <p className={styles.specs_desc}>{deviceById?.ram}</p>
             </div>
           </section>
-        </li>
-      </ul>
-    </div>
+        </div>
+      </div>
+    </>
   );
 };
-
-      {/* <ProductsRow
-        products={recommended}
-        title="You may also like"
-        hasDiscount={true}
-      /> */}
